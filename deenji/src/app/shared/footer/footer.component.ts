c5b3a22310c86d41ef202f12247228be7864b538
alifakheri <alifakheri@protonmail.com>
2025-02-15 03:10:27 +0330

    generates footer w/ nx without style

---
+++ deenji/src/app/shared/footer/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'footer',
+  imports: [CommonModule],
+  templateUrl: './footer.component.html',
+  standalone: true
+})
+export class FooterComponent { }
